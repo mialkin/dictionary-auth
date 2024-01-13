a982b9d887d152f2543938e775998aba73ceacd9
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-13 19:11:37 +0300

    Add Auth controller

---
+++ src/Dictionary.Auth.Controllers/AuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Dictionary.Auth.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(
+        [FromQuery] string username,
+        [FromQuery] string password,
+        CancellationToken cancellationToken)
+    {
+        await Task.Yield();
+
+        return Ok();
+    }
+}
