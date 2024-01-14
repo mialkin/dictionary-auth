b64416f61bf9a757e0dc21266a13b8d6755a9fe0
Aleksei Mialkin <aleksei@mialkin.ru>
2024-01-14 21:41:12 +0300

    Use LoginRequest

---
+++ src/Dictionary.Auth.Controllers/Auth/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Dictionary.Auth.Controllers.Auth.Requests;
+using Dictionary.Auth.UseCases.Auth.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dictionary.Auth.Controllers.Auth;
+
+public class AuthController : ApplicationController
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request, CancellationToken cancellationToken)
+    {
+        var result = await Sender.Send(new LoginCommand(request.Username, request.Password), cancellationToken);
+        return Ok(result);
+    }
+}
---
+++ src/Dictionary.Auth.Controllers/Auth/Requests/LoginRequest.cs
@@ -0,0 +1,3 @@
+namespace Dictionary.Auth.Controllers.Auth.Requests;
+
+public record LoginRequest(string Username, string Password);
